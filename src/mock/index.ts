64637fb327b32828583c429fd48868c6524a3d94
tsiupaknazar <sjprime280@gmail.com>
2022-10-28 11:38:22 +0300

    commit to gh

---
+++ src/mock/index.ts
@@ -0,0 +1,16 @@
+import { LocalGithubUser } from 'types';
+
+export const defaultUser: LocalGithubUser = {
+  "login": "github",
+  "avatar": "https://avatars.githubusercontent.com/u/9919?v=4",
+  "name": "GitHub",
+  "company": "",
+  "blog": "https://github.com/about",
+  "location": "San Francisco, CA",
+  "bio": "How people build software.",
+  "twitter": "",
+  "repos": 436,
+  "followers": 11990,
+  "following": 0,
+  "created": "2008-05-11T04:37:31Z",
+}
